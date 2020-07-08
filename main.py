7751831106ff9568e5b6a33e28367e7499b62295
svetadomoi <heavensfeel@yandex.ru>
2020-07-08 12:22:59 +0300

    edited due to new requirements specification

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
-def CompTimeStampRelative(AcqStartedLine,OriginLine):
+def findAcqStarted(linesArr,CurrentIndex,CurrAcqStarted): # поиск ближайшего Acquisition Started
+	for i in range(CurrentIndex+1,len(linesArr)):
+		if "Acquisition Started" in linesArr[i]:
+			if i - CurrentIndex < CurrentIndex - CurrAcqStarted:
+				return i
+			else:
+				return CurrAcqStarted
+		return CurrAcqStarted
 
+def CompTimeStampRelative(AcqStartedLine,OriginLine):
+	ColonIndex = AcqStartedLine.find(':')
+	if AcqStartedLine[ColonIndex-2].isdigit():
+		Hours = int(AcqStartedLine[ColonIndex-2:ColonIndex])
+	else:
+		Hours = int(AcqStartedLine[ColonIndex-1])
+	Minutes = int(AcqStartedLine[ColonIndex+1:ColonIndex+3])
+	AcqTime = Hours*60*60 + Minutes*60
 	ColonIndex = OriginLine.find(':')
-	if OriginLine[ColonIndex-2].isdigit():
+	if AcqStartedLine[ColonIndex-2].isdigit():
 		Hours = int(OriginLine[ColonIndex-2:ColonIndex])
 	else:
 		Hours = int(OriginLine[ColonIndex-1])
 	Minutes = int(OriginLine[ColonIndex+1:ColonIndex+3])
-
 	OriginTime = Hours*60*60 + Minutes*60
-	minDifference = abs(OriginTime - AcqStartedLine[0])
-	Time = OriginTime - AcqStartedLine[0]
-	for i in range(len(AcqStartedLine)):
-		if abs(OriginTime - AcqStartedLine[i]) < minDifference:
-			minDifference = abs(OriginTime - AcqStartedLine[i])
-			Time = OriginTime - AcqStartedLine[i]
-
-	return Time
+	return OriginTime-AcqTime
 
 def acqSysFormat(CurrAcqLine): # форматирование строки со значением AcquisitionSystem в поле EventSource не с пустым значением FileSizeBytes
 	CurrAcqLine = CurrAcqLine[10:]
 	TempLineForTime = '\t'+CurrAcqLine[CurrAcqLine.find(":")-15:CurrAcqLine.find(":")+4]
-	TempLineFileNumber = CurrAcqLine[CurrAcqLine.find("AcquisitionSystem"):CurrAcqLine.find('\t',CurrAcqLine.find("AcquisitionSystem"))+4]
+	TempLineFileNumber = CurrAcqLine[CurrAcqLine.find("AcquisitionSystem"):CurrAcqLine.find('\t',CurrAcqLine.find("AcquisitionSystem"))+3]
 	TempLineForInflow = CurrAcqLine[CurrAcqLine.find("Inflow",CurrAcqLine.find("AcquisitionSystem")):CurrAcqLine.find(".sgy")+4]
-	if TempLineFileNumber[len(TempLineFileNumber)-2]=='\t':
-		TempLineFileNumber = TempLineFileNumber[:-1]
-	if TempLineFileNumber[len(TempLineFileNumber)-1]!='\t':
-		TempLineFileNumber+='\t'
+	if TempLineFileNumber[len(TempLineFileNumber)-1] != '\t':
+		TempLineFileNumber += '\t'
 	CurrAcqLine = TempLineForTime + TempLineFileNumber + TempLineForInflow
 	return CurrAcqLine
 
@@ -36,48 +41,46 @@ def userLineFormat(CurrUsrLine): # форматирование строки с�
 	usrLine = usrLine[:20]+usrLine[usrLine.find("User"):]
 	return usrLine
 
-def covertAcqStartedToTime(AcqStartedArr,AcqStartedTime):
-	for i in range(len(AcqStartedArr)):
-		AcqStartedLine = AcqStartedArr[i]
-		ColonIndex = AcqStartedLine.find(':')
-		if AcqStartedLine[ColonIndex-2].isdigit():
-			Hours = int(AcqStartedLine[ColonIndex-2:ColonIndex])
-		else:
-			Hours = int(AcqStartedLine[ColonIndex-1])
-		Minutes = int(AcqStartedLine[ColonIndex+1:ColonIndex+3])
-		AcqStartedTime.append(Hours*60*60 + Minutes*60)
-
+	
 with open(r"Example_file.log","r") as datafile:
 	lines = datafile.readlines()
 
+
 AcqStartedArr = []
-AcqStartedTime = []
 for i in range(len(lines)):
 	if "Acquisition Started" in lines[i]:
-		AcqStartedArr.append(lines[i])
-
-covertAcqStartedToTime(AcqStartedArr,AcqStartedTime)
+		AcqStartedArr.append(i)
+AcqStartedArr.append(-1)
 
 outputfile = open('result.txt','w')
 outputfile.write('TimeStampRelative'+'\t'+'RunNumber'+'\t'+'TimeStampLocal'+'\t'+'EventSource'+'\t'+'FileNumber'+'\t'+'EventMessage'+'\n')
-
+j = 0
+k = 0
 for i in range(len(lines)):
+	if i == AcqStartedArr[k]:
+		k+=1
+		if i != AcqStartedArr[0]:
+			j+=1
+			continue
 	if "AcquisitionSystem" in lines[i] and "Acquisition Started" not in lines[i]:
 		AcqSys = i
-		continue
 	if "User" in lines[i] and i != len(lines)-1:
 		if "User" in lines[i+1]:
 			continue
 		else:
-			outputfile.write(str(CompTimeStampRelative(AcqStartedTime,lines[AcqSys]))+'\t'*2)
+			outputfile.write(str(CompTimeStampRelative(lines[AcqStartedArr[j]],lines[AcqSys]))+'\t'*2)
+
 			outputfile.write(acqSysFormat(lines[AcqSys])+'\n')
-			outputfile.write(str(CompTimeStampRelative(AcqStartedTime,lines[i]))+'\t'*2)
+
+			outputfile.write(str(CompTimeStampRelative(lines[AcqStartedArr[j]],lines[i]))+'\t'*2)
+
 			outputfile.write(userLineFormat(lines[i])+'\n')
+
 			if "Acquisition Started" in lines[i+1]:
-				outputfile.write(str(CompTimeStampRelative(AcqStartedTime,lines[i+2]))+'\t'*2)
+				outputfile.write(str(CompTimeStampRelative(lines[i+1],lines[i+2]))+'\t'*2)
 				outputfile.write(acqSysFormat(lines[i+2])+'\n')
 			else:
-				outputfile.write(str(CompTimeStampRelative(AcqStartedTime,lines[i+1]))+'\t'*2)
+				outputfile.write(str(CompTimeStampRelative(lines[AcqStartedArr[j]],lines[i+1]))+'\t'*2)
 				outputfile.write(acqSysFormat(lines[i+1])+'\n')
 
 outputfile.close()
